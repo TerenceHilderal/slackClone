28cd9d1b4091fcd9cdfad97e8c51b6b54737ceef
Hilderal Térence <terencehilderal@gmail.com>
2021-08-29 13:06:00 +0200

    init project with redux template

---
+++ src/Firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+	apiKey: 'AIzaSyCe_bSGWY4EPR33NaBlf_Q7Ja8jF28tEvg',
+	authDomain: 'slack-clone-a6ced.firebaseapp.com',
+	projectId: 'slack-clone-a6ced',
+	storageBucket: 'slack-clone-a6ced.appspot.com',
+	messagingSenderId: '1075943419396',
+	appId: '1:1075943419396:web:73df663ae985e52fad4f51',
+};
